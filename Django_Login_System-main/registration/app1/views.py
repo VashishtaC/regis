29729e5275f2364d9b0cec3c91554c235f84b2bd
VashishtaC <137385579+VashishtaC@users.noreply.github.com>
2023-09-26 16:54:19 +0530

    Add files via upload

---
+++ Django_Login_System-main/registration/app1/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+@login_required(login_url='login')
+def HomePage(request):
+    return render (request,'home.html')
+
+from django.contrib.auth.forms import UserCreationForm
+
+def SignupPage(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def LoginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to the 'home' URL name on successful login
+        else:
+            return HttpResponse("Username or Password is incorrect!!!")
+
+    return render(request, 'login.html')
+
+def LogoutPage(request):
+    logout(request)
+    return redirect('login')
